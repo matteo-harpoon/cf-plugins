7b59e21456831663e707b65045f83837c0e88b9b
Matteo Zambon <matteo@thetophat.org>
2018-02-21 13:01:26 -0300

    Added Package version parser

--- cleanup/cleanup-bash.sh
+++ cleanup/cleanup-bash.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+PKG_VERSION=${cat package.json | grep '"version": "[a-z0-9\.\-]\{5,\}"' | sed 's/"version": "//g' | sed 's/\ //g' | sed 's/",//g'}
+
 echo ""
 echo "Let Airbrake know we've deployed a new version"
 curl -X POST \
   -H "Content-Type: application/json" \
-  -d '{"environment":"'${BLUEMIX_ENV}'","username":"'${AIRBRAKE_USERNAME}'","repository":"'${GIT_URL}'","revision":"'${GIT_COMMIT}'"}' \
+  -d '{"environment":"'${BLUEMIX_ENV}'","username":"'${AIRBRAKE_USERNAME}'","repository":"'${GIT_URL}'","revision":"'${GIT_COMMIT}'","version":"v'${PKG_VERSION}'"}' \
   "https://airbrake.io/api/v4/projects/${AIRBRAKE_PROJECT_ID}/deploys?key=${AIRBRAKE_PROJECT_KEY}"
 
 echo ""
